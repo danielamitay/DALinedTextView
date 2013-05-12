08eafaec570f16e944f7a802acb2d9108158d4e1
danielamitay <hello@danielamitay.com>
2013-05-12 19:04:31 -0400

    Create example project

---
+++ DALinedTextViewExample/DALinedTextViewExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DALinedTextViewExample
+//
+//  Created by Daniel Amitay on 5/12/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DALinedTextViewExample/DALinedTextViewExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DALinedTextViewExample
+//
+//  Created by Daniel Amitay on 5/12/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
