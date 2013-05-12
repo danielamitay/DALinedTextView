c78a20f4ebeaf68196fab80d6e4b19eca9336289
danielamitay <hello@danielamitay.com>
2013-05-12 18:54:36 -0400

    Create base class files

---
+++ DALinedTextView/DALinedTextView.h
@@ -0,0 +1,13 @@
+//
+//  DALinedTextView.h
+//  DALinedTextView
+//
+//  Created by Daniel Amitay on 5/12/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DALinedTextView : UITextView
+
+@end
---
+++ DALinedTextView/DALinedTextView.m
@@ -0,0 +1,31 @@
+//
+//  DALinedTextView.m
+//  DALinedTextView
+//
+//  Created by Daniel Amitay on 5/12/13.
+//  Copyright (c) 2013 Daniel Amitay. All rights reserved.
+//
+
+#import "DALinedTextView.h"
+
+@implementation DALinedTextView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
