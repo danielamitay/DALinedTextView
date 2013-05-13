2a35902038dabb045d8e9bba35c92330ce88b030
danielamitay <hello@danielamitay.com>
2013-05-13 13:17:42 -0400

    Add conditionals for UIColor validity

--- DALinedTextView/DALinedTextView.m
+++ DALinedTextView/DALinedTextView.m
@@ -23,26 +23,34 @@
 {
     // Drawing code
     CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, self.horizontalLineColor.CGColor);
     CGContextSetLineWidth(context, 1.0f);
-    CGContextBeginPath(context);
     
-    CGFloat baseOffset = 8.0f + self.font.descender;
-    NSUInteger numberOfLines = (self.contentSize.height + self.bounds.size.height) / self.font.lineHeight;
-    for (NSInteger line = 0; line < numberOfLines; ++line)
+    if (self.horizontalLineColor)
     {
-        CGFloat linePointY = (baseOffset + (self.font.lineHeight * line));
-        CGContextMoveToPoint(context, self.bounds.origin.x, linePointY);
-        CGContextAddLineToPoint(context, self.bounds.size.width, linePointY);
+        CGContextSetStrokeColorWithColor(context, self.horizontalLineColor.CGColor);
+        CGContextBeginPath(context);
+        
+        CGFloat baseOffset = 8.0f + self.font.descender;
+        NSUInteger numberOfLines = (self.contentSize.height + self.bounds.size.height) / self.font.lineHeight;
+        for (NSInteger line = 0; line < numberOfLines; ++line)
+        {
+            CGFloat linePointY = (baseOffset + (self.font.lineHeight * line));
+            CGContextMoveToPoint(context, self.bounds.origin.x, linePointY);
+            CGContextAddLineToPoint(context, self.bounds.size.width, linePointY);
+        }
+        
+        CGContextClosePath(context);
+        CGContextStrokePath(context);
     }
-    CGContextClosePath(context);
-    CGContextStrokePath(context);
     
-    CGContextSetStrokeColorWithColor(context, self.verticalLineColor.CGColor);
-    CGContextMoveToPoint(context, -1.0f, -self.bounds.size.height);
-    CGContextAddLineToPoint(context, -1.0f, self.contentSize.height + self.bounds.size.height);
-    CGContextClosePath(context);
-    CGContextStrokePath(context);
+    if (self.verticalLineColor)
+    {
+        CGContextSetStrokeColorWithColor(context, self.verticalLineColor.CGColor);
+        CGContextMoveToPoint(context, -1.0f, -self.bounds.size.height);
+        CGContextAddLineToPoint(context, -1.0f, self.contentSize.height + self.bounds.size.height);
+        CGContextClosePath(context);
+        CGContextStrokePath(context);
+    }
 }
 
 #pragma mark - Property methods
