59c80e9cc273d7f522a761b34b45191ba984bf8c
danielamitay <hello@danielamitay.com>
2013-05-21 23:24:39 -0400

    Set zeroed offset to 7 so that the font sits on the lines

--- DALinedTextView/DALinedTextView.m
+++ DALinedTextView/DALinedTextView.m
@@ -76,7 +76,7 @@
         CGContextSetStrokeColorWithColor(context, self.horizontalLineColor.CGColor);
         // Create un-mutated floats outside of the for loop.
         // Reduces memory access.
-        CGFloat baseOffset = 8.0f + self.font.descender;
+        CGFloat baseOffset = 7.0f + self.font.descender;
         CGFloat screenScale = [UIScreen mainScreen].scale;
         CGFloat boundsX = self.bounds.origin.x;
         CGFloat boundsWidth = self.bounds.size.width;
