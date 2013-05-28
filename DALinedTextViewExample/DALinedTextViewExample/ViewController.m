446e203ed894eeea1d74eda2590e1bdae0ea1c37
danielamitay <hello@danielamitay.com>
2013-05-28 06:15:22 -0400

    Add readme text to example project

--- DALinedTextViewExample/DALinedTextViewExample/ViewController.m
+++ DALinedTextViewExample/DALinedTextViewExample/ViewController.m
@@ -25,6 +25,15 @@
     self.textView.frame = self.view.bounds;
     self.textView.autoresizingMask = (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth);
     [self.view addSubview:self.textView];
+    
+    self.textView.text =
+    @"DALinedTextView"
+    @"\n\n\n"
+    @"`DALinedTextView` is a `UITextView` subclass that draws ruled lines to the view, similar to iOS' built-in Notes app. The lines conform to the appropriate line-height for the currently set `UIFont`."
+    @"\n\n"
+    @"`DALinedTextView` is special because it was built with performance and appropriate behavior in mind. Most Q&A and OSS solutions involve image-based backgrounds or overzealous drawing code. For most applications such implementations are great, but `DALinedTextView` was built with 1000-line (or more) `UITextView`s in mind."
+    @"\n\n"
+    @"Performance decisions are commented so as to provide insight into the thought process behind `DALinedTextView`. The current `-drawRect:` implementation takes as little a 1ms on an iPhone 5.";
 }
 
 - (void)didReceiveMemoryWarning
